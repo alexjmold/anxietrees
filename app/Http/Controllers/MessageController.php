d03063b787cd5725dac08bb7106c41e924e54689
alexjmold <alexjmold@gmail.com>
2025-07-27 20:46:14 +0100

    Removed unused facades

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,4 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Message;
-use App\Models\Tree;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 class MessageController extends Controller {}
